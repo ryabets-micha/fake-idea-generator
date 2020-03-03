a2d6fcd43df6928b9d7e396397932884d0006ed2
m.ryabets <m.ryabets@adus-technologies.com>
2020-03-03 18:11:13 +0100

    Added script to fill the database with fake data (users, ideas)

---
+++ index.js
@@ -0,0 +1,53 @@
+const axios = require('axios');
+const faker = require('faker');
+
+const IDEA_GENERATOR = 'https://appideagenerator.com/call.php';
+const IDEA_API = 'http://localhost:4000';
+
+const randomInt = () => Math.floor(Math.random() * 10);
+
+const generateIdea = async () => {
+    const { data } = await axios.get(IDEA_GENERATOR);
+    return data.replace(/\n/g, '');
+};
+
+const generateUser = async () => {
+    const { data } = await  axios.post(`${IDEA_API}/register`, {
+        username: faker.internet.userName(),
+        password: 'password'
+    });
+
+    return data.token;
+};
+let count = 0;
+const postNewIdea = async token => {
+    const idea = await generateIdea();
+    const { data } = await axios.post(`${IDEA_API}/api/ideas`, {
+        idea,
+        description: faker.lorem.paragraph()
+    }, {
+        headers: { authorization: `Bearer ${token}` }
+    });
+
+    // console.log(data);
+    console.log(++count);
+    return idea;
+};
+
+(async () => {
+    const start = new Date().getTime();
+
+    const randUserNum = randomInt();
+    const randIdeaNum = randomInt();
+
+    for (let i = 0; i < randUserNum; i++) {
+        const token = await generateUser();
+
+        for (let j = 0; j < randIdeaNum; j++) {
+            const idea = await postNewIdea(token);
+        }
+    }
+
+    const end = new Date().getTime();
+    console.log(`SecondWay: ${end - start}ms`)
+})();
